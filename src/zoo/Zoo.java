b4d984f89f96470f067fa286485ce06be2653a96
dominicmaki <45606075+dominicmaki@users.noreply.github.com>
2018-12-04 13:22:56 -0600

    Update Zoo.java

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -10,7 +10,25 @@ public class Zoo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+              Scanner Bob =  new Scanner(System.in);
+        int choice;
+        String SectionChoice = "What section would you like to see first?\n" 
+                + "1: Fire Nation"
+                + "2: Water Tribe"
+                + "3: Air Nomads";
+        System.out.println(SectionChoice);
+        choice = Bob.nextInt();
+        switch(choice){
+            case 1:
+                   break;
+            case 2:
+                   break;
+            case 3:
+                   break;
+            default: System.out.println("Please retry");
+                   break;
+                   
+        }
     }
     
 }
