d7e63cc21f0f284a92964ec0239f1b488a3a4f45
mmireles5341 <mmireles5341@007LAB183003353.neisd.ad>
2018-12-05 23:15:16 -0600

    quavo done

--- src/zoo/Quablaze.java
+++ src/zoo/Quablaze.java
@@ -5,80 +5,39 @@ package zoo;
  * @author mmireles5341
  */
 class Quablaze extends LaFlame {
-    private String Color;
-    private boolean LivesinWater = true;
-    private String Diet;
-    private boolean BreatheAir = true;
+    private boolean YellowFur = true;
+    private boolean Mammal = true;
+    private boolean Nocturnal = true;
     
     
     
-    public static void BreatheAir()
+    public static void Yellow()
     {
-        System.out.println("I'm a dolphin and I breathe air!");
+        System.out.println("My fur is mostly yellow but it has some spots of black mixed in...IT WOULD BE SO COOL IF I COULD FIND A SONG ABOUT THE COLORS OF MY FUR... oh well");
     }
     
-    public void Color()
+    public void Mammal()
     {
-        System.out.println("MY COLOR IS " + getColor());
+        System.out.println("Yes I am a mammal which is pretty nice because fun fact I have a FEAR OF EGGS");
     }
-    public void Diet()
+    public void Nocturnal()
     { 
-        System.out.println("I eat " + getDiet());
+        System.out.println("I guess you could say I'm more a night owl (hoot hoot) lol that's code for I'm nocturnal!!");
     }  
     
-    public void Play(){
-        System.out.println("I love to play!!!!!! WOOOOOOOOOOO!!!!");
+    public void Hunt(){
+        System.out.println("It gets lonely out there at night by myself, so I prefer to hunt in packs:))");
     }
     
-    public void Party(){
-        System.out.println("I love to PARTAYYY! *dolphin noise*");
+    public void Migrate(){
+        System.out.println("Betcha didn't know I migrate to the rainforest in the winter!! Yeah I like it down there it's just so...TROPICAL");
     }
     
-    public void Bowling(){
-        System.out.println("I use the 4lb ball when I go bowling!! *dolphin noise*");
+    public void Sleeps(){
+        System.out.println("Since I'm still part of the fire nation, I must do something weird with fire right?? Welp I sleep in ashes of brush I find, but it's not that weird! It's kinda comfortable!");
     }
 
-    /**
-     * @return the Color
-     */
-    public String getColor() {
-        return Color;
-    }
 
-    /**
-     * @param Color the Color to set
-     */
-    public void setColor(String Color) {
-        this.Color = Color;
-    }
-
-    /**
-     * @return the LivesinWater
-     */
-    public boolean isLivesinWater() {
-        return LivesinWater;
-    }
-
-    /**
-     * @return the Diet
-     */
-    public String getDiet() {
-        return Diet;
-    }
-
-    /**
-     * @param Diet the Diet to set
-     */
-    public void setDiet(String Diet) {
-        this.Diet = Diet;
-    }
-
-    /**
-     * @return the BreatheAir
-     */
-    public boolean isBreatheAir() {
-        return BreatheAir;
-    }
 
     
 }
